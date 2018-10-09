fca61ca10e6022ff15f6bfbc9b03a3a747700680
r888800009 <r888800009@users.noreply.github.com>
2018-10-10 04:43:50 +0800

    Update test script

--- DS3/test.sh
+++ DS3/test.sh
@@ -3,6 +3,8 @@ g++ main.cpp -o main
 for i in {1..10}
 do
     str=$(./testGen.py)
+    echo
     echo $str
-    printf "%d\n%s\n" 1 "$str" | ./main
+    echo
+    printf "1\n%s\n" "$str" | ./main
 done
