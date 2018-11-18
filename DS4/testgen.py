2437a2d703c79b7c31914635deef5ae12ab80f39
r888800009 <r888800009@users.noreply.github.com>
2018-11-18 11:46:13 +0800

    Add DS4 testgen.py

---
+++ DS4/testgen.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import random
+for i in range(1, 9):
+    fileout = open("input{}.txt".format(i), "w")
+    print("OID\tArrival\tDuration\tTimeOut", file=fileout)
+    oids = list(range(100, 300))
+    random.shuffle(oids)
+    for oid in oids:
+        arrival = random.randint(1, 2000)
+        duration = random.randint(1, 600)
+        timeout = arrival + random.randint(1, 600)
+        print("{}\t{}\t{}\t{}".format(oid, arrival, duration, timeout), file=fileout)
+
