9e78f060c4119428e559b44a3c4612bd58b2322d
r888800009 <r888800009@users.noreply.github.com>
2018-11-24 02:19:07 +0800

    Update test.sh

--- DS4/test.sh
+++ DS4/test.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
-g++ main_kao.cpp ; cat test.txt |./a.out;
+g++ main_kao.cpp
 
-for num in 401 402 1000 2000
-do
-    vimdiff ./sort$num.txt ./out/sort$num.txt
-    vimdiff ./one$num.txt ./out/one$num.txt
-    vimdiff ./two$num.txt ./out/two$num.txt
-done
+cat test.txt | ./a.out >stdout;
 
+function cmpfiles ()
+{
+    { echo "$1" ; diff ./$1 <(sed 's/\r//g' ./out/$1 ) ; } |less
+}
 
+function auto ()
+{
+    for num in 1000 2000
+    do
+        cmpfiles sort$num.txt
+        cmpfiles one$num.txt
+        cmpfiles two$num.txt
+    done
+}
+
+function manual ()
+{
+    echo "manual"
+    while true
+    do
+        read -p "fileNum:" num
+        echo $num
+
+        cmpfiles sort$num.txt
+        cmpfiles one$num.txt
+        cmpfiles two$num.txt
+    done
+}
+
+if [ $1 = "man" ]
+then
+    manual
+else
+    auto
+fi
