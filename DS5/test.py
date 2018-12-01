0086e8eac143b557d9f7f4cbd66a3be135c7ad98
r888800009 <r888800009@users.noreply.github.com>
2018-12-01 23:15:40 +0800

    Add test.py

---
+++ DS5/test.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import sys
+import csv
+select = 8
+
+def checkFile(file1):
+    first = True
+    pre = 0
+    count = 1
+    with open(file1, encoding='big5', newline='') as csvfile:
+        spamerader = csv.reader(csvfile, delimiter='\t')
+        for row in spamerader:
+            cur = int(row[select])
+            if first is True:
+                first = False
+                pre = cur
+            else:
+                if pre < cur:
+                    print(' '.join(row))
+                    print(file1 + " bug🐞line " + str(count))
+                    print("pre: {} cur: {}".format(pre, cur))
+                pre = cur
+            count += 1
+
+
+for file1 in sys.argv[1:]:
+    print(file1)
+    checkFile(file1)
+
+
