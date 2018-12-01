296c590c6468919e685a1624954470fa5f15b86e
r888800009 <r888800009@users.noreply.github.com>
2018-12-01 23:57:50 +0800

    Update test.sh

--- DS5/test.sh
+++ DS5/test.sh
@@ -4,9 +4,10 @@ rm *_sort*.txt
 files=( 1 201 202 203 204 205 501 502 503 )
 for i in "${files[@]}"
 do
-    echo 1 $i |./a.out
-    echo 2 $i |./a.out
-    echo 3 $i |./a.out
+    echo "***$i***"
+    echo 1 $i |./a.out| grep 'ms'
+    echo 2 $i |./a.out| grep 'ms'
+    echo 3 $i |./a.out| grep 'ms'
 done
 
 ./test.py *_sort*.txt
